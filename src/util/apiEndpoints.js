7439d448ef04842cf11a2c0a94112762d14528be
abhey <abheykumar7860@gmail.com>
2025-11-15 00:05:57 +0530

    Fixed 403 error by dynamically mapping context-path and adjusting filter order

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://financial-tracking-app-yust.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://financial-tracking-app-yust.onrender.com/api/v1.0";
+// export const BASE_URL = "http://localhost:8080/api/v1.0";
 
 const CLOUDINARY_CLOUD_NAME = "dkgwgormx";
 
